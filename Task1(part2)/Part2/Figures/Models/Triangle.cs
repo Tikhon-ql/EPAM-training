582e32f124bfafc70950c6e975721fd49031575d
Tikhon-ql <tihkon.grek@mail.ru>
2020-07-12 15:43:15 +0300

    commit

--- Task1(part2)/Part2/Figures/Models/Triangle.cs
+++ Task1(part2)/Part2/Figures/Models/Triangle.cs
@@ -49,5 +49,6 @@ namespace Figures.Models
         {
             return base.Equals(obj);
         }
+       
     }
 }
--- Task1(part2)/Part2/Part2.Tests/Part2Tests.cs
+++ Task1(part2)/Part2/Part2.Tests/Part2Tests.cs
@@ -13,13 +13,16 @@ namespace Part2.Tests
     [TestClass]
     public class Part2Tests
     {
+        /// <summary>
+        /// Проверка чтения из файла
+        /// </summary>
         [TestMethod]
         public void ReadFromFile_Should_Return_TextContext()
         {
             //arrange
             List<Figure> figures = new List<Figure>();
             string filename = @"D:\Тихон\Git\EPAM-training\Task1(part2)\Part2\Part2\Resources\test.txt";
-            string expected = "Triangle;10;15;41Square;15Rectangle;16;4Triangle;12;20;8";
+            string expected = "Triangle;10;15;12Square;15Rectangle;16;4Triangle;11;11;18";
 
             //act
 
@@ -34,12 +37,15 @@ namespace Part2.Tests
             //assert
             Assert.AreEqual(expected,actual);
         }
+        /// <summary>
+        /// Нахождение фигур равных Sqaure(10)
+        /// </summary>
         [TestMethod]
         public void Equals_Square_10_Recatangle_15_25_Triangle_12_20_8returned()
         {
             //arrange
             List<Figure> figures = new List<Figure>();
-            string expected = "Rectangle;16;4Triangle;12;20;8";
+            string expected = "Rectangle;16;4Triangle;11;11;18";
             string filename = @"D:\Тихон\Git\EPAM-training\Task1(part2)\Part2\Part2\Resources\test.txt";
             Square square = new Square(10);
 
@@ -52,6 +58,29 @@ namespace Part2.Tests
                 {
                     actual += item.ToString();
                 }
+            }
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        /// <summary>
+        /// Тест S всех фигур
+        /// </summary>
+        [TestMethod]
+        public void S()
+        {
+            //arrange
+            List<Figure> figures = new List<Figure>();
+            string expected = "59,8116836412419;225;64;56,9209978830308;";
+            string filename = @"D:\Тихон\Git\EPAM-training\Task1(part2)\Part2\Part2\Resources\test.txt";
+            Square square = new Square(10);
+
+            //act
+            figures = FileWorker.ReadFromFile(filename);
+            string actual = "";
+            foreach (Figure item in figures)
+            {
+                actual += item.S() + ";";
             }
 
             //assert
