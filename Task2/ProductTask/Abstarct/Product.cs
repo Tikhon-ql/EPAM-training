ae71cf9589c9af73fc6741be9e624fca76d80f89
Tikhon-ql <tihkon.grek@mail.ru>
2020-07-17 12:42:07 +0300

    commit

---
+++ Task2/ProductTask/Abstarct/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductTask.Abstarct
+{
+    public abstract class Product
+    {
+        public string Name { get; set; }
+        public string Kind { get; set; }
+        public decimal Price { get; set; }
+
+        protected Product(string name, string kind, decimal price)
+        {
+            Name = name;
+            Kind = kind;
+            Price = price;
+        }
+        /// <summary>
+        /// Перобразование к int
+        /// </summary>
+        /// <param name="product"></param>
+        public static explicit operator int(Product product)
+        {
+            return Convert.ToInt32(product.Price) * 100;
+        }
+        /// <summary>
+        /// Преобразование к double
+        /// </summary>
+        /// <param name="product"></param>
+        public static explicit operator double(Product product)
+        {
+            return Convert.ToDouble(product.Price) * 100;
+        }
+    }
+}
---
+++ Task2/ProductTask/Models/FoodProducts.cs
@@ -0,0 +1,38 @@
+﻿using ProductTask.Abstarct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductTask.Models
+{
+    public class FoodProducts : Product
+    {
+        public FoodProducts(string name,decimal price):base(name,"Пищевые",price){}
+        /// <summary>
+        /// Преобразование из ELectriaclaPRoducts к FoodProducts
+        /// </summary>
+        /// <param name="product"></param>
+        public static explicit operator FoodProducts(ELectricalProducts product)
+        {
+            return new FoodProducts(product.Name,product.Price);
+        }
+        /// <summary>
+        /// Преобразование из StationeryProducts к FoodProducts
+        /// </summary>
+        /// <param name="product"></param>
+        public static explicit operator FoodProducts(StationeryProducts product)
+        {
+            return new FoodProducts(product.Name, product.Price);
+        }
+        /// <summary>
+        /// Перегрузка оператора сложения
+        /// </summary>
+        public static FoodProducts operator +(FoodProducts f1,FoodProducts f2)
+        {
+            return new FoodProducts(f1.Name + "-" + f2.Name, (f1.Price + f2.Price)/2);
+        }
+
+    }
+}
